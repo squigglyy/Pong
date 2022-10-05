e85a79fd6bc487073dc38bfb40e8b7c668d21a5e
squigglyy <79401750+squigglyy@users.noreply.github.com>
2022-10-05 19:57:43 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle 
+
+screen = turtle.Screen()
+screen.title("Pong By Squiggly")
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+
+
+while True:
+    screen.update()
+    
